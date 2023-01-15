9ab3ec6c303b5169cb6006d59cd5d93370635ad1
luc-git <102831178+luc-git@users.noreply.github.com>
2023-01-15 22:06:51 +0100

    ajout menu hamburger éléments inutiles de la page retirés et avancement sur le responsive

--- script.js
+++ script.js
@@ -17,12 +17,19 @@ document.getElementById("seconds").innerText = seconds + " Seconde(s)"
 },1000)
 
 const onglet = document.getElementById("mobile-onglet")
+const nav = document.querySelector("nav")
 
 onglet.addEventListener("click", event => {
     if(document.getElementById("onglets").style.visibility == "visible"){
-        document.getElementById("onglets").style.visibility = "hidden" 
+        document.getElementById("onglets").style.visibility = "hidden"
+        document.getElementById("buttons").style.visibility = "hidden"
+        nav.style.visibility = "collapse"
+        document.body.style.overflow = "scroll"
     }
     else{
-        document.getElementById("onglets").style.visibility = "visible" 
+        document.getElementById("onglets").style.visibility = "visible"
+        document.getElementById("buttons").style.visibility = "visible"
+        nav.style.visibility = "visible"
+        document.body.style.overflow = "hidden"
     }
 })
