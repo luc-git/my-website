024edfe9de1550e9903f56be8e5599c57df55f65
luc-git <102831178+luc-git@users.noreply.github.com>
2023-02-01 19:40:51 +0100

    Changement url onglets navigation corrections d'erreurs js et ajout du 0 au timer

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-setInterval(() => {
-    const toDay = new Date().getTime()
-    const event = new Date("2023-2-18 14:00:00").getTime() //+ 50400000
-    const difference = event - toDay
-
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000)
-
-    document.getElementById("days").innerText = days + " J"
-    document.getElementById("hours").innerText = hours + " H"
-    document.getElementById("minutes").innerText = minutes + " M"
-    document.getElementById("seconds").innerText = seconds + " S"
-
-}, 1000)
-
 const onglet = document.getElementById("mobile-onglet")
 const nav = document.querySelector("nav")
 
@@ -31,11 +14,3 @@ onglet.addEventListener("click", event => {
         document.body.style.overflow = "hidden"
     }
 })
-
-function recaptcha() {
-    const recaptcha = document.forms["captcha"]["g-recaptcha-response"].value
-    if (recaptcha == "") {
-        alert("veuillez remplir le captcha")
-        return false
-    }
-}
